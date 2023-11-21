aa936390e15642fc86e9c39656c4de318bb9c2ab
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2023-11-21 07:45:11 -0500

    primera version

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,5 @@
-public class Autos {
-
-    //atributos
-    String color;
-    String dueno;
-    String modelo;
-    int ano;
-
-    //Constructor
-    Autos(String dueno){
-        this.dueno=dueno;
+public class Main {
+    public static void main(String []args){
+        System.out.println("\tBIENVENIDO");
     }
-
-    //Metodos
-    public String getColor(){
-        return color;
-    }
-
-    public String getModelo(){
-        return modelo;
-    }
-
-    public int getAno(){
-        return ano;
-    }
-
-    //------------------------------------------------
-    //
-    public void setDueno(String dueno) {
-        this.dueno = dueno;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
 }
