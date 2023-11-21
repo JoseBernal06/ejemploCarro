5ab1295a69720489622961108fc4aca038e52aaa
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2023-11-21 09:00:06 -0500

    Cuarta version

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,9 @@ public class Main {
         auto_uno.setModelo("Ferrari");
         auto_uno.setAno(5);
 
+        System.out.println(auto_uno.getModelo());
+        System.out.println(auto_uno.getColor());
+        System.out.println(auto_uno.getAno());
+
     }
 }
