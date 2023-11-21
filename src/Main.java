b12336c7cd79ed1512aaeb42c959be5607179055
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2023-11-21 07:45:11 -0500

    Segunda version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String []args){
         System.out.println("\tBIENVENIDO");
+
+        Autos auto_uno;
+        auto_uno = new Autos("Mateo");
+
+        auto_uno.setColor("Azul");
+        auto_uno.setModelo("Ferrari");
+        auto_uno.setAno(5);
+
     }
 }
