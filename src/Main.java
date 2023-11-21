0520978a8244fd6e3ccf6171dfdc6549a6714836
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2023-11-21 07:45:11 -0500

    Scanner version

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String []args){
+
+        Scanner entrada =new Scanner(System.in);
+
         System.out.println("\tBIENVENIDO");
 
         Autos auto_uno,auto_dos;
         auto_uno = new Autos("Mateo");
         auto_dos = new Autos("Emilio");
 
+        System.out.print("Ingresa el color de tu auto: "); auto_uno.setColor(entrada.next());
+        System.out.print("Ingresa el modelo de tu auto: "); auto_uno.setModelo(entrada.next());
+        System.out.print("Ingresa el ano de compra de tu auto: "); auto_uno.setAno(entrada.nextInt());
+
+        System.out.println(auto_uno.getColor());
+        System.out.println(auto_uno.getAno());
+        System.out.println(auto_uno.getModelo());
+
+        /*auto_uno.setColor("Azul");
+        auto_uno.setModelo("Ferrari");
+        auto_uno.setAno(5);
+
         auto_dos.setColor("Negro");
         auto_dos.setModelo("Nissan");
         auto_dos.setAno(2);
 
-        System.out.println();
+        System.out.println("\nAuto de mateo");
+        System.out.println(auto_uno.getModelo());
+        System.out.println(auto_uno.getAno());*/
+
     }
 }
